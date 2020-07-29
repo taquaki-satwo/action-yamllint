ed3ae50dd729c5900912f14ef3518e702c3d6fa0
Takaaki Sato <taquaki-satwo@users.noreply.github.com>
2020-07-30 05:14:09 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 yamllint --version
 
 yamllint --format "parsable" ${INPUT_YAMLLINT_FLAGS:-'.'} \
-  | reviewdog -efm="%f:%l:%c: %m" -name "yamllint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+  | reviewdog -efm="%f:%l:%c: %m" -name "yamllint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}" -fail-on-error
